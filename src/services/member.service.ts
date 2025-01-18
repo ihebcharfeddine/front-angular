dc5f71681c6e152ffb9f8954c5fadb17ec8fe43c
Oussama Slimani <slimani.oussama@enis.tn>
2025-01-18 10:38:01 +0200

    added first dahsboard

--- src/services/member.service.ts
+++ src/services/member.service.ts
@@ -91,6 +91,24 @@ export class MemberService {
     );
   }
 
+    // get the distribution of students by their diplomas
+    getStudentDistributionByDiploma(): Observable<any> {
+      return this.getAllStudents().pipe(
+        map((students) => {
+          const distribution: { [key: string]: number } = {}; 
+
+          students.forEach((student) => {
+            if (student.diplome) {
+              const diploma = student.diplome as string;  
+              distribution[diploma] = (distribution[diploma] || 0) + 1;
+            }
+          });
+
+          return distribution;
+        })
+      );
+    }
+  
   tabpub: number[] = [];
 
   async getNbStudByTeacher() {
